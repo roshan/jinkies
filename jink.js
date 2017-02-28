c6049b7bcf6f3fabf77a0ac644f2a2cceab30f24
Roshan George <rgeorge@liveramp.com>
2017-02-28 21:55:59 +0000

    Initial commit

---
+++ jink.js
@@ -0,0 +1,39 @@
+function replaceJiraTicketWithUrl(obj) {
+  var jiraPattern = /[A-Z]+-[0-9]+/g;
+
+  chrome.storage.sync.get({
+    githubUrl: '',
+    jiraUrl: ''
+  }, function(items) {
+    var githubUrl = items.githubUrl;
+    var jiraUrl = items.jiraUrl;
+    var hostname = document.location.hostname
+
+    if (githubUrl.includes(hostname)) {
+      var jiraUrlString = jiraUrl + "/secure/QuickSearch.jspa?searchString=";
+
+      var matched = obj.html().match(jiraPattern);
+
+      if ( matched ) {
+	var text = obj.html();
+	$.each(matched, function (i, v) {
+	  text = text.replace(v, "<a href='"+jiraUrlString+v+"'>"+v+"</a>");
+	});
+	obj.html(text);
+	return obj
+      }
+    } else {
+      console.log(githubUrl + " did not match " + document.location.hostname);
+    }
+
+  })
+}
+
+$( ".commit-desc" ).each(function() {
+  return replaceJiraTicketWithUrl($(this));
+})
+
+$( ".commit-title" ).each(function() {
+  return replaceJiraTicketWithUrl($(this));
+})
+
---
+++ options.js
@@ -0,0 +1,31 @@
+// Saves options to chrome.storage
+function save_options() {
+  var githubUrl = document.getElementById('github-url').value;
+  var jiraUrl = document.getElementById('jira-url').value;
+  chrome.storage.sync.set({
+    githubUrl: githubUrl,
+    jiraUrl: jiraUrl
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    githubUrl: '',
+    jiraUrl: ''
+  }, function(items) {
+    document.getElementById('github-url').value = items.githubUrl;
+    document.getElementById('jira-url').value = items.jiraUrl;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
